7a9d1d96da0f04ce4bc9e4859f6aaa6cb8d1c297
Luis Pedro Coelho <luis@luispedro.org>
2024-02-22 17:06:28 +1000

    MIN Make room for alternative preprocessing styles

--- preprocess.py
+++ preprocess.py
@@ -3,7 +3,9 @@ from os import makedirs, path
 
 NGLESS_THREADS = 4
 
-def preprocess(dataset, sample, compression: str = 'zst') -> str:
+def preprocess(dataset, sample, compression: str = 'zst', preprocess: str = '25q/45ell') -> str:
+    if preprocess != '25q/45ell':
+        raise NotImplementedError('Only 25q/45ell is supported')
 
     oname = f'preproc-data/{dataset}/{sample}.preproc.fq.{compression}'
     makedirs(path.dirname(oname), exist_ok=True)
